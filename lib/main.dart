b459a655a9d2b309a063b88fe633584151b4b6ef
Mahmudul_Hasan6 <mukto2004@gmail.com>
2026-01-17 19:21:15 +0000

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart';
 
-import 'package:helloworld/gradient_container.dart';
-
-void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(
-          colors: [Colors.black, Colors.white],
-        ),
-      ),
-    ),
-  );
-}
